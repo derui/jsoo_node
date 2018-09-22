82082b5b1f87a5a6707ba82353d3f639141c8159
derui <derutakayu@gmail.com>
2018-09-22 15:25:22 +0900

    chore: Avoid warning in jsoo runtime

--- src/inner_util.ml
+++ src/inner_util.ml
@@ -1,3 +1,2 @@
 (** require module as commonjs style. *)
-let require module_ : 'a Js.t =
-  Js.Unsafe.pure_js_expr @@ "require('" ^ module_ ^ "')"
+let require module_ : 'a Js.t = Js.Unsafe.js_expr ("require('" ^ module_ ^ "')")
